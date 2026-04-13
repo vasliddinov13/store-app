f1169347d21da6730d04e0c4f32f2060f7831388
vasliddinov13 <nurmuhammadvasliddiniv03@gmail.com>
2026-04-13 18:17:56 +0500

    error page finished

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,35 @@
+import { Link, useRouteError } from "react-router-dom";
+
 const Error = () => {
-    return <h1 className="text-4xl">Error</h1>
+
+    const error = useRouteError();
+
+    if (error.status === 404) {
+        return <main className="min-h-[100vh] grid place-items-center px-8">
+            <div className="text-center">
+                <p className="text-9xl font-semibold text-primary">
+                    404
+                </p>
+                <h1 className="text-3xl font-bold traching-tight mt-4 sm:text-5xl">
+                    page not found
+                </h1>
+                <p className="mt-6 text-lg leading-7">
+                    Sorry, we couldn’t find the page you’re looking for.
+                </p>
+                <div className="mt-10">
+                    <Link to='/' className="btn btn-secondary">
+                        GO BACK HOME
+                    </Link>
+                </div>
+            </div>
+        </main>
+    }
+
+    return <main className="min-h-[100vh] grid place-items-center px-8">
+        <h4 className="text-4xl text-center font-bold">
+            there was an error...
+        </h4>
+    </main>
 }
 
 export default Error;
